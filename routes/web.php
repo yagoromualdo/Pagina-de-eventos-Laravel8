f6909c7a54fb4aa480e46f6a7a8a7bb0513175c8
Yago Romualdo <yagoromualdo.yr@gmail.com>
2021-10-13 11:23:30 -0300

    13/10/21

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+
+    $nome = "Yago";
+    $idade = 18;
+
+    $arr = [1,2,3,4,5];
+
+    $nomes = ["Yago", "Maria", "João", "Ana"];
+
+    return view('welcome',
+    ['nome' => $nome,
+    'idade' => $idade,
+    'arr' => $arr,
+    'nomes' => $nomes
+]);
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+
+Route::get('/produtos', function () {
+    return view('products');
+});
